6ec26ae4f09ad619cb10004ef0d5d0501acf2465
kongr45gpen <electrovesta@gmail.com>
2019-12-23 00:27:13 +0200

    Use ports actually on the Nucleo

--- Src/stm32h7xx_hal_msp.c
+++ Src/stm32h7xx_hal_msp.c
@@ -396,19 +396,19 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
     /**TIM2 GPIO Configuration    
-    PA3     ------> TIM2_CH4
+    PA0     ------> TIM2_CH1
     PB10     ------> TIM2_CH3
-    PA15 (JTDI)     ------> TIM2_CH1
+    PB11     ------> TIM2_CH4
     PB3 (JTDO/TRACESWO)     ------> TIM2_CH2 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_15;
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_3;
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_3;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
